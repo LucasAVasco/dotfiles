99c41ec138b33c6bcced78dbe2edc2f434cb9c0b
LucasAVasco <67849851+LucasAVasco@users.noreply.github.com>
2024-06-03 04:15:55 -0300

    feat(Nvim): add color schemes
    Add these colorschemes: 'rebelot/kanagawa.nvim',
    'EdenEast/nightfox.nvim', 'catppuccin/nvim'
    
    They are lazy loaded

--- nvim/.config/nvim/lua/lazy_plugins/colorschemes.lua
+++ nvim/.config/nvim/lua/lazy_plugins/colorschemes.lua
@@ -2,5 +2,17 @@ return {
 	{
 		'ellisonleao/gruvbox.nvim',
 		lazy = true,
+	},
+	{
+		'rebelot/kanagawa.nvim',
+		lazy = true,
+	},
+	{
+		'EdenEast/nightfox.nvim',
+		lazy = true,
+	},
+	{
+		'catppuccin/nvim',
+		lazy = true,
 	}
 }
