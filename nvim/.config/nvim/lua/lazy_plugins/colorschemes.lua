fcdc97dc867351a86d3117c39803934ea7a717b8
LucasAVasco <67849851+LucasAVasco@users.noreply.github.com>
2024-06-03 04:13:06 -0300

    chore(Nvim): move color scheme selection to init
    Select the color scheme in the '~/.config/nvim/init.lua' file.
    Lazy load all color schemes

--- nvim/.config/nvim/lua/lazy_plugins/colorschemes.lua
+++ nvim/.config/nvim/lua/lazy_plugins/colorschemes.lua
@@ -1,17 +1,6 @@
 return {
 	{
 		'ellisonleao/gruvbox.nvim',
-
-		lazy = false,
-		priority = 8500,
-
-		config = function()
-			vim.api.nvim_exec([[
-			syntax enable
-			colorscheme gruvbox
-			]],
-			false -- do not return anything
-			)
-		end
+		lazy = true,
 	}
 }
