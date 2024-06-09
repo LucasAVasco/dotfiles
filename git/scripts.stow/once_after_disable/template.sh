2b187ac67ff15b9ba31832413f37ee8e857bdc30
LucasAVasco <67849851+LucasAVasco@users.noreply.github.com>
2024-06-09 14:38:41 -0300

    feat(git): scripts to initial template and hooks
    Add scripts to enable and disable the installation of git hooks and
    the repository template.
    
    The template is in the '~/.config/git/template', and the hooks are in
    the '~/.config/git/template/hooks' directory

---
+++ git/scripts.stow/once_after_disable/template.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+
+git config --global --unset init.templateDir
---
+++ git/scripts.stow/once_after_enable/template.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+
+# Sets the template directory to all newly created repositories
+git config --global init.templateDir ~/.config/git/template
