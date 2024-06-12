c5d6924da938110250988c473de5c9d905138188
LucasAVasco <67849851+LucasAVasco@users.noreply.github.com>
2024-06-12 18:13:13 -0300

    feat(bspwm): move command to clear clipboard
    Move the command to the new '~/.config/clipboard/clear.sh' script

---
+++ clipboard/.config/clipboard/clear.sh
@@ -0,0 +1,15 @@
+#!/bib/bash
+#
+# Script to clear the user clipboard
+
+
+# Clears the clipboard in a Xorg session
+if [[ -z "$WAYLAND_DISPLAY" ]]; then
+	echo '' | xclip -i -sel primary
+	echo '' | xclip -i -sel secondary
+	echo '' | xclip -i -sel clipboard
+
+# Clears the clipboard in a Wayland session
+else
+	echo 'to do'
+fi
