8e4a7d903b268c467dbe1d73a76ee383758fe25c
LucasAVasco <67849851+LucasAVasco@users.noreply.github.com>
2024-06-13 19:21:16 -0300

    feat(nvim): lazy load 'nvim-notify'

--- nvim/.config/nvim/lua/lazy_plugins/notification.lua
+++ nvim/.config/nvim/lua/lazy_plugins/notification.lua
@@ -1,6 +1,20 @@
+-- Override the default `vim.notify` function to a function that loads 'nvim-notify' before showing the notification.
+-- This allows the 'nvim-notify' plugin to be lazy loaded. It will be loaded only when the first notification be sent
+vim.notify = function(msg, level, opts)
+	require('notify')
+
+	vim.notify(msg, level, opts)
+end
+
+
 return {
 	{
 		'rcarriga/nvim-notify',
+		lazy = true,
 		priority = 9500,
+
+		config = function()
+			vim.notify = require('notify')
+		end
 	}
 }
