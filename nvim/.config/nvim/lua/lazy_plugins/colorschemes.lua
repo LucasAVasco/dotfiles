65a231aec7a63b7c5a0c2a25131bf3d31470de26
LucasAVasco <67849851+LucasAVasco@users.noreply.github.com>
2024-07-05 21:11:36 -0300

    feat(nvim): background transparency
    Configure the color schemes to have transparent background. This works
    better with terminal emulators that have padding and margins. A fully
    filled background creates sharp edges that will be annoying if the
    window has rounded edges

--- nvim/.config/nvim/lua/lazy_plugins/colorschemes.lua
+++ nvim/.config/nvim/lua/lazy_plugins/colorschemes.lua
@@ -1,18 +1,33 @@
 return {
 	{
 		'ellisonleao/gruvbox.nvim',
+		opts = {
+			transparent_mode = true,
+		},
 		lazy = true,
 	},
 	{
 		'rebelot/kanagawa.nvim',
+		opts = {
+			transparent = true,
+		},
 		lazy = true,
 	},
 	{
 		'EdenEast/nightfox.nvim',
+		opts = {
+			options = {
+				transparent = true,
+			}
+		},
 		lazy = true,
 	},
 	{
 		'catppuccin/nvim',
+		main = 'catppuccin',
+		opts = {
+			transparent_background = true,
+		},
 		lazy = true,
 	}
 }
