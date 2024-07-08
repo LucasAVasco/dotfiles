88ef9bf4dbd9eb4073556f4b0ce09a376a81a3d3
LucasAVasco <67849851+LucasAVasco@users.noreply.github.com>
2024-07-07 21:46:18 -0300

    feat(nvim): add `todo-comments` plugin
    Change the highlight and search patterns to allow TODO comments with the
    author between parenthesis. See
    'https://docs.astral.sh/ruff/rules/missing-todo-author/' for further
    information

--- nvim/.config/nvim/lua/lazy_plugins/comments.lua
+++ nvim/.config/nvim/lua/lazy_plugins/comments.lua
@@ -26,5 +26,59 @@ return {
 			vim.g.NERDTrimTrailingWhitespace = 1 -- Remove trailing spaces after remove a comment
 			vim.g.NERDDefaultAlign = 'left'
 		end,
+	},
+	{
+		'folke/todo-comments.nvim',
+		dependencies = {
+			'nvim-lua/plenary.nvim',
+		},
+
+		opts = {
+			highlight = {
+				-- Pattern used to highlight the TODO keyword. It is a Vim regex. The official documentation does not explains how to
+				-- customize this regex, so I checked the source code (at commit 74abcc81254feee74aa129c0a219750686ebcefd). The method that
+				-- implements the match system is the `M.match()` at the 'lua/todo-comments/highlight.lua' file.
+				--
+				-- This pattern is `very magic` and `case insensitive`. See `:h \v` and `:h \C` for further information. It will be used in
+				-- the `vim.fn.matchlist()` function, that returns a list (table in Lua) with the following content:
+				--
+				-- * first element: full matched text
+				--
+				-- * second and next elements: sub-matches of the full match, in the order that they are found in the string: \1, \2, and so
+				-- on
+				--
+				-- The first sub-match (second element) is the content that will be highlighted. The second sub-match (third element) must
+				-- be the keyword (INFO, FIX, etc.). If there are not a second sub-match, the first will be used as the keyword
+				--
+				-- The following modification allows to add the author of the TODO between parenthesis after the TODO keyword. This is used
+				-- by `ruff` as described in the TD002 rule (https://docs.astral.sh/ruff/rules/missing-todo-author/). But can be used in
+				-- languages other than Python. Example:
+				--
+				-- INFO(LucasAVasco): my INFO comment
+				--
+				-- The first sub-match will match the keyword and author between parenthesis if provided. So it will also be highlighted.
+				-- `(\(.{-}\))?` matches, optionally, anything between parenthesis (less as possible) and the parenthesis itself. The second
+				-- sub-match is defined by `(KEYWORDS)`. It will return the keyword required by `todo-comments`
+				pattern = [[.*((KEYWORDS)(\(.{-}\))?)\s*:]],
+			},
+
+			search = {
+				-- Pattern used by Ripgrep (https://github.com/BurntSushi/ripgrep) to search by TODO comments occurrences in the files. Used
+				-- by the following commands: `TodoQuickFix`, `TodoLocList`, `TodoTelescope` and `Trouble todo`. This regex is not used to
+				-- highlight the occurrences on the viewer (E.g. Telescope). The `highlight.pattern` option is used to this
+				--
+				-- It is a rust regex (https://docs.rs/regex/latest/regex/)
+				--
+				-- The following modification allows to add the author of the TODO between parenthesis after the TODO keyword. This is used
+				-- by `ruff` as described in the TD002 rule (https://docs.astral.sh/ruff/rules/missing-todo-author/). But can be used in
+				-- languages other than Python. Example:
+				--
+				-- INFO(LucasAVasco): my INFO comment
+				--
+				-- `(\(.*\))?` matches, optionally, everything between parenthesis and the parenthesis itself. `\b` matches a word boundary
+				-- (position between a word and a non-word match)
+				pattern = [[\b(KEYWORDS)(\(.*\))?\s*:]],
+			}
+		}
 	}
 }
