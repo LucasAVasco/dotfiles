39716f7b0813c4e2588fc990214af39b9556f06f
LucasAVasco <67849851+LucasAVasco@users.noreply.github.com>
2024-07-07 23:30:37 -0300

    chore(nvim): shortcuts in start screen plugin
    In shortcut entries, place the `key` parameter as the second element of
    an entry. Before this commit, it was the last element in the entry.
    Placing it at the beginning of the entry is useful to check if a key is
    already in use

--- nvim/.config/nvim/lua/lazy_plugins/start_screen.lua
+++ nvim/.config/nvim/lua/lazy_plugins/start_screen.lua
@@ -20,17 +20,17 @@ return {
 				},
 
 				shortcut = {
-					{ icon = ' ', icon_hl = icon_hl, desc = 'Plugins', group = desc_hl, action = 'Lazy', key = 'p' },
-					{ icon = ' ', icon_hl = icon_hl, desc = 'New file', group = desc_hl, action = 'new | only', key = 'n' },
-					{ icon = ' ', icon_hl = icon_hl, desc = 'File tree', group = desc_hl, action = 'NvimTreeFocus', key = 't' },
-					{ icon = '󰖌 ', icon_hl = icon_hl, desc = 'Oil nvim', group = desc_hl, action = 'Oil', key = 'o' },
-					{ icon = ' ', icon_hl = icon_hl, desc = 'Find file', group = desc_hl, action = 'Telescope find_files', key = 'f' },
-					{ icon = ' ', icon_hl = icon_hl, desc = 'Git status', group = desc_hl, action = 'Git | only', key = 'g' },
-					{ icon = '󰸟 ', icon_hl = icon_hl, desc = 'Healthchecks', group = desc_hl,
-						action = 'Lazy load all | checkhealth', key = 'h' },
-					{ icon = ' ', icon_hl = icon_hl, desc = 'Check key maps', group = desc_hl,
-						action = 'Lazy load all | checkhealth which-key', key = 'k' },
-					{ icon = '󱎘 ', icon_hl = icon_hl, desc = 'Quit', group = desc_hl, action = 'quit', key = 'q' },
+					{ icon = ' ', key = 'p', icon_hl = icon_hl, desc = 'Plugins', group = desc_hl, action = 'Lazy'},
+					{ icon = ' ', key = 'n', icon_hl = icon_hl, desc = 'New file', group = desc_hl, action = 'new | only'},
+					{ icon = ' ', key = 't', icon_hl = icon_hl, desc = 'File tree', group = desc_hl, action = 'NvimTreeFocus'},
+					{ icon = '󰖌 ', key = 'o', icon_hl = icon_hl, desc = 'Oil nvim', group = desc_hl, action = 'Oil'},
+					{ icon = ' ', key = 'f', icon_hl = icon_hl, desc = 'Find file', group = desc_hl, action = 'Telescope find_files'},
+					{ icon = ' ', key = 'g', icon_hl = icon_hl, desc = 'Git status', group = desc_hl, action = 'Git | only'},
+					{ icon = '󰸟 ', key = 'h', icon_hl = icon_hl, desc = 'Healthchecks', group = desc_hl,
+						action = 'Lazy load all | checkhealth'},
+					{ icon = ' ', key = 'k', icon_hl = icon_hl, desc = 'Check key maps', group = desc_hl,
+						action = 'Lazy load all | checkhealth which-key'},
+					{ icon = '󱎘 ', key = 'q', icon_hl = icon_hl, desc = 'Quit', group = desc_hl, action = 'quit'},
 				},
 
 				packages = { enable = true }  -- Show installed packages
