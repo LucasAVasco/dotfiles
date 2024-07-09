836274ed92031fbb6f2a8089fe943898661793b7
LucasAVasco <67849851+LucasAVasco@users.noreply.github.com>
2024-07-09 14:24:38 -0300

    feat(nvim): replace `vim-surround` plugin by `nvim-surround`
    `nvim-surround` is written in Lua

--- nvim/.config/nvim/lua/lazy_plugins/surrounds.lua
+++ nvim/.config/nvim/lua/lazy_plugins/surrounds.lua
@@ -1,3 +1,16 @@
 return {
-	'tpope/vim-surround',
+	'kylechui/nvim-surround',
+	version = '*',
+	keys = {
+		'cs', 'cS' , 'ys', 'yS', 'ds', 'dS',                  -- Normal mode
+		{ '<C-g>s' , mode = 'i' }, { '<C-g>S' , mode = 'i' }, -- Insert mode
+		{ 'S' , mode = 'v' }, { 'gS' , mode = 'v' }           -- Visual mode
+	},
+
+	opts = {
+		-- Highlights the surrounds to be replaced in normal mode
+		highlight = {
+			duration = 700  -- milliseconds
+		}
+	}
 }
