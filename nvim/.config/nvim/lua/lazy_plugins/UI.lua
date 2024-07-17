9cbf31d1ffc048557de71a0d2b64af595b96d16f
LucasAVasco <67849851+LucasAVasco@users.noreply.github.com>
2024-07-17 17:44:15 -0300

    docs(nvim): add classes to the `opts` arguments in 'UI.lua' functions
    Add classes to document the following functions at the 'UI.lua' file:
    
    * `vim.ui.input`
    
    * `vim.ui.select`
    
    The classes document the fields of the `opts` table passed to these
    functions

--- nvim/.config/nvim/lua/lazy_plugins/UI.lua
+++ nvim/.config/nvim/lua/lazy_plugins/UI.lua
@@ -1,8 +1,13 @@
+---@class vim_ui_input_opts
+---@field prompt? string Title of the prompt
+---@field default? string Start value of the prompt
+---@field completion? string Auto-completion type to use in the input. Same as on the `:help command-completion` page. E.g. 'dir'
+
 --- Shows the input pop up with `dressing.nvim` plugin
 --- Override the default `vim.ui.input` function to a function that loads `dressing.nvim` before showing the pop up. This allows the
 --- `dressing.nvim` plugin to be lazy loaded. The first call to this function will load the `dressing.nvim` plugin, and it will replace the
 --- function by the one provided by this plugin
----@param opts table Options to be sent to `dressing.nvim`
+---@param opts vim_ui_input_opts Options to send to `dressing.nvim`
 ---@param callback_input fun(text: string|nil) Function called after the user provide the input. Receives the user input as parameter
 ---@diagnostic disable-next-line: duplicate-set-field
 vim.ui.input = function(opts, callback_input)
@@ -12,12 +17,17 @@ vim.ui.input = function(opts, callback_input)
 end
 
 
+---@class vim_ui_select_opts
+---@field prompt? string Title of the prompt
+---@field format_item? fun(item: string): string Function that overrides the appearance of the item in the UI
+---@field kind? string Information related to the prompt type. Managed by `dressing.nvim`
+
 --- Shows the select pop up with `dressing.nvim` plugin
 --- Override the default `vim.ui.select` function to a function that loads `dressing.nvim` before showing the pop up. This allows the
 --- `dressing.nvim` plugin to be lazy loaded. The first call to this function will load the `dressing.nvim` plugin, and it will replace the
 --- function by the one provided by this plugin
----@param item_list table Tens that the user can select
----@param opts table Options to send to `dressing.nvim`
+---@param item_list table<string> Items that the user can select
+---@param opts vim_ui_select_opts Options to send to `dressing.nvim`
 ---@param callback_select fun(selected_item: string|nil, item_index: number|nil)  Function called after the user select a item
 ---@diagnostic disable-next-line: duplicate-set-field
 vim.ui.select = function(item_list, opts, callback_select)
