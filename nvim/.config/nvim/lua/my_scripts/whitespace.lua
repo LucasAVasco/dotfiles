55d38c17d719b67a56236b1ec195dfefeaf05963
LucasAVasco <67849851+LucasAVasco@users.noreply.github.com>
2024-07-27 21:06:48 -0300

    fix(nvim): can not highlight white space error
    Bug: the white space highlight utility does not works if the user open
    the buffer in a window whose last buffer should not highlight them. E.g.
    You open a help menu (should not highlight white space errors) and,
    after this, open a file in the same window. The desired behavior is t
    enable the white space error highlight. But the old configuration does
    not enables it
    
    Cause: set the `__whitespace_error_loaded` flag before checking if
    should load the white space errors highlights. This enables the flag even
    if the highlights are not applied
    
    Solution: set the `__whitespace_error_loaded` flag after checking if
    should load the highlights in the current window

--- nvim/.config/nvim/lua/my_scripts/whitespace.lua
+++ nvim/.config/nvim/lua/my_scripts/whitespace.lua
@@ -6,23 +6,23 @@ vim.api.nvim_create_autocmd({ 'BufWinEnter', 'BufNewFile'}, {
 		local buffer_nr = arguments.buf
 		local window_id = MYFUNC.get_window_by_buffer(buffer_nr)
 
-		-- Only add the matches one time per window
-		if vim.w[window_id].__whitespace_error_loaded then
-			return
-		else
-			vim.w[window_id].__whitespace_error_loaded = true
-		end
-
-		-- Only add the matches to normal buffers (bufftype == '')
+		-- Only adds the matches to normal buffers (bufftype == '')
 		if vim.bo[buffer_nr].buftype ~= '' then
 			return
 		end
 
-		-- The filetype will be text, c, cpp, editable files. If Neovim is started without a file, this option will be empty
-		-- and the matches will not be added. This prevents the matches from being added to the start screen, but requires that
-		-- the user provides a file to edit. Otherwise, the matches will not be added
+		-- The filetype will be text, c, cpp, editable files. If Neovim is started without a file, this option will be empty and the matches
+		-- will not be added. This prevents the matches from being added to the start screen, but requires that the user provides a file to
+		-- edit. Otherwise, the matches will not be added
 		if vim.bo[buffer_nr].filetype == '' then
 			return
+		end
+
+		-- Only adds the matches one time per window
+		if vim.w[window_id].__whitespace_error_loaded then
+			return
+		else
+			vim.w[window_id].__whitespace_error_loaded = true
 		end
 
 		local priority = 1000
