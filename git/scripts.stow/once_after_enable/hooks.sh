c3f92414e43f7b38c62c1776ba48b0d83013d7a6
LucasAVasco <67849851+LucasAVasco@users.noreply.github.com>
2024-08-15 20:40:29 -0300

    feat(git): add global git configuration file
    Remove some scripts. They are no longer necessary:
    
    * 'git/scripts.stow/once_after_disable/hooks.sh'
    
    * 'git/scripts.stow/once_after_disable/template.sh'
    
    * 'git/scripts.stow/once_after_enable/hooks.sh'
    
    * 'git/scripts.stow/once_after_enable/template.sh'

--- git/scripts.stow/once_after_enable/hooks.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-
-# Uses the hooks in the template directory instead of the default ones, so does not need to update the hooks in the local repositories
-# every time the hooks in the template directory are updated
-git config --global core.hooksPath ~/.config/git/template/hooks
--- git/scripts.stow/once_after_enable/template.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-
-# Sets the template directory to all newly created repositories
-git config --global init.templateDir ~/.config/git/template
