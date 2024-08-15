5463b548fbd5b92e0c1948d583322d93abce1130
LucasAVasco <67849851+LucasAVasco@users.noreply.github.com>
2024-08-15 19:46:55 -0300

    refactor(nvim): `MYPATHS` variable
    Optimize the `MYPATHS` global variable: single call to
    `vim.fn.stdpath('data')`
    
    Remove these unused paths:
    
    * plugins_LSP
    
    * plugins_local
    
    Add `mason.nvim` packages path

--- nvim/.config/nvim/lua/my_scripts/paths.lua
+++ nvim/.config/nvim/lua/my_scripts/paths.lua
@@ -1,15 +1,19 @@
----@type table<string>
-_G.MYPATHS = {
+local data = vim.fn.stdpath('data') --[[@as string]]
+
+---@type table<string, string>
+MYPATHS = {
 	home = vim.env.HOME,
 	config = vim.env.HOME .. '/.config/nvim/',
-	dev = vim.env.HOME .. '/Repositories/neovim_repos',            -- Development folder (used to load plugins in 'dev' mode)
-	data = vim.fn.stdpath('data'),
-	lazy_nvim = vim.fn.stdpath('data') .. '/lazy/lazy.nvim',       -- Where to install Lazy.nvim
+	dev = vim.env.HOME .. '/Repositories/neovim_repos/', -- Development folder (used to load plugins in 'dev' mode)
+	data = data,
+	lazy_nvim = data .. '/lazy/lazy.nvim/', -- Where to install Lazy.nvim
 
 	-- Plugins
 	plugins = vim.env.HOME .. '/.config/nvim/lua/lazy_plugins/',
-	plugin_empty = vim.env.HOME .. '/.config/nvim/empty_plugin/',  -- Folder to be used as 'dir' in local plugins
+	plugin_empty = vim.env.HOME .. '/.config/nvim/empty_plugin/', -- Folder to be used as 'dir' in local plugins
 	plugins_ft = vim.env.HOME .. '/.config/nvim/lua/lazy_plugins/filetype/',
-	plugins_LSP = vim.env.HOME .. '/.config/nvim/lua/lazy_plugins/LSP',
-	plugins_local = vim.env.HOME .. '/.config/nvim/local_plugins/',
+
+	-- `mason.nvim`
+	mason = data .. '/mason/',
+	mason_packages = data .. '/mason/packages/',
 }
