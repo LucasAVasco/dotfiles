6681c180baeed43dd051aa6eed61ec634cda5036
LucasAVasco <67849851+LucasAVasco@users.noreply.github.com>
2024-08-18 07:05:44 -0300

    fix(nvim): Neovim can not update the last color scheme
    Bug: if the user removes the current color scheme (uninstall t, by
    example). The next Neovim session causes an error, and the user can not
    restore the last color scheme anymore
    
    Cause: the 'colorscheme.lua' script tries to load the last selected
    color scheme. If the user removes it, the color scheme update will throw
    an error and exit the script. So the auto-command to save the selected
    color scheme will not be loaded
    
    Solution: load the auto-command that saves the current color scheme
    before applying it. Set the current color scheme with `pcall()` and show
    a notification if Neovim can not load it

--- nvim/.config/nvim/lua/my_scripts/colorscheme.lua
+++ nvim/.config/nvim/lua/my_scripts/colorscheme.lua
@@ -6,26 +6,33 @@
 	It also creates an automatic command to save the color scheme name whenever the user changes it
 ]]
 
+local file_path = MYPATHS.data .. '/last_colorsheme' -- File to save the current color scheme name
 
-local file_path = MYPATHS.data .. '/last_colorsheme'  -- File to save the current color scheme name
-
+-- Auto-command to save the color scheme name after the user select it
+vim.api.nvim_create_autocmd('ColorScheme', {
+	callback = function(arguments)
+		local file = io.open(file_path, 'w')
+		if file ~= nil then
+			file:write(arguments.match)
+			file:close()
+		end
+	end,
+})
 
 -- Load the last selected color-scheme
 local file = io.open(file_path, 'r')
 if file ~= nil then
 	local colorscheme = file:read('*a')
-	vim.cmd.colorscheme(colorscheme)
 	file:close()
-end
 
+	-- Color scheme update
 
--- Auto-command to save the color scheme name after the user select it
-vim.api.nvim_create_autocmd('ColorScheme', {
-	callback = function(arguments)
-		file = io.open(file_path, 'w')
-		if file ~= nil then
-			file:write(arguments.match)
-			file:close()
-		end
+	local success, result = pcall(vim.cmd.colorscheme, colorscheme)
+
+	if not success then
+		vim.notify(
+			('Can not load the color scheme: %s. Result: %s'):format(colorscheme, vim.inspect(result)),
+			vim.log.levels.ERROR
+		)
 	end
-})
+end
