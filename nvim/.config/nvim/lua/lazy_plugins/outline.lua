8437d92e37927751a9ab60bc658d9f73e196dbb2
LucasAVasco <67849851+LucasAVasco@users.noreply.github.com>
2024-10-17 14:38:24 -0300

    feat(nvim): replace 'tagbar' by 'aerial.nvim'
    Aerial uses LSP and treesitter.

---
+++ nvim/.config/nvim/lua/lazy_plugins/outline.lua
@@ -0,0 +1,70 @@
+return {
+	{
+		'stevearc/aerial.nvim',
+		dependencies = {
+			-- Optional
+			'nvim-treesitter/nvim-treesitter',
+			'nvim-tree/nvim-web-devicons',
+		},
+
+		cmd = {
+			'AerialClose',
+			'AerialCloseAll',
+			'AerialGo',
+			'AerialInfo',
+			'AerialNavClose',
+			'AerialNavOpen',
+			'AerialNavToggle',
+			'AerialNext',
+			'AerialOpen',
+			'AerialOpenAll',
+			'AerialPrev',
+			'AerialToggle',
+		},
+
+		keys = {
+			{ '<leader>go', '<CMD>AerialToggle<CR>', desc = 'Go to outline window' },
+			{ '<leader>gn', '<CMD>AerialNavToggle<CR>', desc = 'Go to navigation window' },
+		},
+
+		opts = {
+			keymaps = {
+				['l'] = 'actions.jump',
+				['<tab>'] = 'actions.jump',
+			},
+
+			autojump = true,
+			close_on_select = true,
+
+			-- Navigation window
+			nav = {
+				preview = true,
+				keymaps = {
+					['q'] = 'actions.close',
+					['<tab>'] = 'actions.jump',
+				},
+
+				-- Appearance {{{
+
+				min_height = { 30, 0.9 },
+				max_width = 0.6,
+				min_width = { 0.5, 30 },
+
+				-- }}}
+			},
+
+			-- Appearance {{{
+
+			layout = {
+				min_width = 30,
+			},
+
+			show_guides = true,
+			guides = {
+				last_item = '╰─',
+			},
+
+			-- }}}
+		},
+	},
+}
