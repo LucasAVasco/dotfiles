98806f3fdbf78dc0cf4621df62f29bbc1ab044e9
LucasAVasco <67849851+LucasAVasco@users.noreply.github.com>
2024-10-17 16:54:06 -0300

    chore(clipboard): improve comments

--- clipboard/.config/clipboard/clear.sh
+++ clipboard/.config/clipboard/clear.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #
-# Script to clear the user clipboard
+# Clear the user clipboard
 
 
-# Clears the clipboard in a Xorg session
+# Xorg session
 if [[ -z "$WAYLAND_DISPLAY" ]]; then
 	echo '' | xclip -i -sel primary
 	echo '' | xclip -i -sel secondary
 	echo '' | xclip -i -sel clipboard
 
-# Clears the clipboard in a Wayland session
+# Wayland session
 else
 	wl-copy --clear
 	wl-copy --primary --clear
