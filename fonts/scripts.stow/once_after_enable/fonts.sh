0fe9444ef163a6213cb92626faf23347208e3936
LucasAVasco <67849851+LucasAVasco@users.noreply.github.com>
2024-12-14 16:03:33 -0300

    feat(fonts): add Google's Noto Emoji font installation

--- fonts/scripts.stow/once_after_enable/fonts.sh
+++ fonts/scripts.stow/once_after_enable/fonts.sh
@@ -3,7 +3,8 @@
 # Install some nerd fonts inside the '$fonts_folder' folder. Use the `download_font()` function to install the desired fonts
 
 
-version=v3.2.1                       # Version of the fonts
+nerdfonts_version=v3.2.1             # Version of the fonts from Nerd Fonts
+google_emoji_version=v2.047          # Version of the Google's Emoji Fonts
 fonts_folder=~/.fonts/auto_installed # Install the fonts inside this folder
 
 
@@ -32,9 +33,32 @@ download_font () {
 	echo -e "\n\e[1;32m'$1' version '$2' Installed\e[1;0m"
 }
 
+# Install Google's Noto Emoji fonts
+#
+# Download from the GitHub repository. The fount will be installed inside a sub-folder in the '$fonts_folder' directory
+#
+# $2: Version of the font to download
+download_emoji_font() {
+	# Folder to hold this specific font
+	install_folder="$fonts_folder/google-noto-emoji"
+
+	# Download
+	curl --location "https://codeload.github.com/googlefonts/noto-emoji/zip/refs/tags/$1" --output "$tmp_dir/emoji.zip"
+	rm -r "$tmp_dir/emoji"
+	unzip "$tmp_dir/emoji.zip" -d "$tmp_dir/emoji"
+
+	# (Re)Installation
+	rm -r "$install_folder" 2> /dev/null
+	mkdir -p "$install_folder"
+	mv "$tmp_dir"/emoji/noto-emoji-*/fonts/* "$install_folder"
+
+	# Log message
+	echo -e "\n\e[1;32m'Google Noto Emoji' version '$2' Installed\e[1;0m"
+}
 
 # Removes old fonts
 rm -r "$fonts_folder" 2> /dev/null
 
 # Installs the desired fonts
-download_font Hack $version
+download_font Hack $nerdfonts_version
+download_emoji_font $google_emoji_version
