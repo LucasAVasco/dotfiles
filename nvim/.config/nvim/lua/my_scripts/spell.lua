fc2c987c7aafd51bf1c669609c0c36616162284d
LucasAVasco <67849851+LucasAVasco@users.noreply.github.com>
2025-01-11 16:51:38 -0300

    feat(nvim): enable CamelCase support to spell checker

--- nvim/.config/nvim/lua/my_scripts/spell.lua
+++ nvim/.config/nvim/lua/my_scripts/spell.lua
@@ -2,6 +2,7 @@ vim.opt.spell = true
 vim.opt.spelllang = 'en_us'
 vim.opt.spellfile = MYPATHS.config .. 'spell_adds/main.UTF-8.add'
 vim.opt.complete:append('k')
+vim.opt.spelloptions:append('camel')
 
 -- Local dictionaries and thesaurus
 vim.opt.dictionary:append('.vim/local_dictionary')
