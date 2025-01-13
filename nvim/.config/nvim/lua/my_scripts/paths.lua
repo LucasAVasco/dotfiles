c9b0a464e39c19599ce846928bc3028a38f1aa01
LucasAVasco <67849851+LucasAVasco@users.noreply.github.com>
2025-01-12 23:12:25 -0300

    refactor(nvim): use local variables to set `MYPATHS` global variable
    Use local variables to set the paths of `MYPATHS` global variable. Avoid
    code repetition

--- nvim/.config/nvim/lua/my_scripts/paths.lua
+++ nvim/.config/nvim/lua/my_scripts/paths.lua
@@ -1,19 +1,24 @@
 local data = vim.fn.stdpath('data') --[[@as string]]
 
+local home = vim.env.HOME
+local config = home .. '/.config/nvim/'
+local config_lua = config .. '/lua/'
+local mason = data .. '/mason/'
+
 ---@type table<string, string>
 MYPATHS = {
-	home = vim.env.HOME,
-	config = vim.env.HOME .. '/.config/nvim/',
-	dev = vim.env.HOME .. '/Repositories/neovim_repos/', -- Development folder (used to load plugins in 'dev' mode)
+	home = home,
+	config = config,
+	dev = home .. '/Repositories/neovim_repos/', -- Development folder (used to load plugins in 'dev' mode)
 	data = data,
 	lazy_nvim = data .. '/lazy/lazy.nvim/', -- Where to install Lazy.nvim
 
 	-- Plugins
-	plugins = vim.env.HOME .. '/.config/nvim/lua/lazy_plugins/',
-	plugin_empty = vim.env.HOME .. '/.config/nvim/empty_plugin/', -- Folder to be used as 'dir' in local plugins
-	plugins_ft = vim.env.HOME .. '/.config/nvim/lua/lazy_plugins/filetype/',
+	plugins = config_lua .. '/lazy_plugins/',
+	plugin_empty = config .. '/empty_plugin/', -- Folder to be used as 'dir' in local plugins
+	plugins_ft = config_lua .. '/lazy_plugins/filetype/',
 
 	-- `mason.nvim`
-	mason = data .. '/mason/',
-	mason_packages = data .. '/mason/packages/',
+	mason = mason,
+	mason_packages = mason .. '/packages/',
 }
