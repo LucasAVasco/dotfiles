7948fa509e264af835d0c957799f6ab84ac5b595
LucasAVasco <67849851+LucasAVasco@users.noreply.github.com>
2025-01-20 19:06:28 -0300

    feat(nvim)!: add key-bind to set database connection of current buffer
    Use the '<leader>MB' key-bind to set the database connection to use on
    the current buffer
    
    BREAKING CHANGE: replace the keybind to open Dadbod UI from '<leader>M'
    to '<leader>MB'

--- nvim/.config/nvim/lua/lazy_plugins/database.lua
+++ nvim/.config/nvim/lua/lazy_plugins/database.lua
@@ -1,3 +1,40 @@
+---Set the database connection to use on the current buffer
+local function set_current_buffer_database_conn()
+	local conn_list = vim.fn['db_ui#connections_list']()
+
+	---List of open connections
+	---@type string[]
+	local open_conn_list = {}
+	for _, conn in ipairs(conn_list) do
+		if conn.is_connected == 1 then
+			table.insert(open_conn_list, conn.name)
+		end
+	end
+
+	if #open_conn_list == 0 then
+		vim.notify('There are not open connections!', vim.log.levels.ERROR)
+		return
+	end
+
+	-- The user should choose the connection to use on the current buffer
+	vim.ui.select(open_conn_list, {
+		prompt = 'Select the connection to use on the current buffer',
+	}, function(selected_item, _)
+		if selected_item == nil then
+			return
+		end
+
+		for _, conn in ipairs(conn_list) do
+			if conn.name == selected_item then
+				vim.b.db = conn.url
+				return -- Does not show the error message
+			end
+		end
+
+		vim.notify('The selected connection does not exist in the DBUI connection list: ' .. selected_item, vim.log.levels.ERROR)
+	end)
+end
+
 return {
 	{
 		'tpope/vim-dadbod',
@@ -17,7 +54,8 @@ return {
 		},
 
 		keys = {
-			{ '<leader>M', '<CMD>DBUIToggle<CR>' },
+			{ '<leader>MU', '<CMD>DBUIToggle<CR>', desc = 'Toggle database User interface' },
+			{ '<leader>MB', set_current_buffer_database_conn, desc = 'Set current buffer database connection' },
 		},
 
 		cmd = {
@@ -48,45 +86,12 @@ return {
 				end,
 			})
 
-			-- Command to set the current buffer database
-			vim.api.nvim_create_user_command('DBUISetBufferCompletion', function()
-				local conn_list = vim.fn['db_ui#connections_list']()
-
-				---List of open connections
-				---@type string[]
-				local open_conn_list = {}
-				for _, conn in ipairs(conn_list) do
-					if conn.is_connected == 1 then
-						table.insert(open_conn_list, conn.name)
-					end
-				end
-
-				if #open_conn_list == 0 then
-					vim.notify('There are not open connections!', vim.log.levels.ERROR)
-					return
-				end
-
-				-- The user should choose the connection to use on the current buffer
-				vim.ui.select(open_conn_list, {
-					prompt = 'Select the connection to use on the current buffer',
-				}, function(selected_item, _)
-					if selected_item == nil then
-						return
-					end
-
-					for _, conn in ipairs(conn_list) do
-						if conn.name == selected_item then
-							vim.b.db = conn.url
-							return -- Does not show the error message
-						end
-					end
-
-					vim.notify(
-						'The selected connection does not exist in the DBUI connection list: ' .. selected_item,
-						vim.log.levels.ERROR
-					)
-				end)
-			end, {})
+			-- Command to set the current buffer database connection
+			vim.api.nvim_create_user_command(
+				'DBUISetBufferCompletion',
+				set_current_buffer_database_conn,
+				{ desc = 'set the current buffer database connection' }
+			)
 		end,
 	},
 }
