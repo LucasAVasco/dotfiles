0b7a9a788148fd63a153536f864b13e137f8f8d3
LucasAVasco <67849851+LucasAVasco@users.noreply.github.com>
2025-02-09 20:26:11 -0300

    feat(nvim): add 'vim-table-mode' plugin

---
+++ nvim/.config/nvim/lua/lazy_plugins/writing.lua
@@ -0,0 +1,20 @@
+return {
+	{
+		'dhruvasagar/vim-table-mode',
+
+		cmd = {
+			'TableAddFormula',
+			'TableSort',
+			'Tableize',
+			'TableModeRealign',
+			'TableModeToggle',
+			'TableEvalFormulaLine',
+			'TableModeDisable',
+			'TableModeEnable',
+		},
+
+		keys = {
+			{ '<leader>t', desc = 'Table mode mappings' },
+		},
+	},
+}
