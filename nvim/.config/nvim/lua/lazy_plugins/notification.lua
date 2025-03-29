ad3eadfb6a073aa1e430767c9b6f7466b5aafd02
LucasAVasco <67849851+LucasAVasco@users.noreply.github.com>
2025-03-29 12:55:18 -0300

    chore(nvim): fix annotations

--- nvim/.config/nvim/lua/lazy_plugins/notification.lua
+++ nvim/.config/nvim/lua/lazy_plugins/notification.lua
@@ -3,8 +3,9 @@
 --- 'nvim-notify' plugin to be lazy loaded. The first call to this function will load the `nvim-notify` plugin, and my `lazy.nvim`
 --- configuration will replace the function by the one provided by this plugin
 ---@param msg string Body of the message
----@param level? number|string Notification level number (vim.log.levels) or a string with the name of the level. E.g. 'error'.
+---@param level? number Notification level number (vim.log.levels) or a string with the name of the level. E.g. 'error'.
 ---@param opts? notify.Options Options table to send to `nvim-notify` plugin
+---@module 'nvim-notify'
 ---@diagnostic disable-next-line: duplicate-set-field
 vim.notify = function(msg, level, opts)
 	require('notify')
