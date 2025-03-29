be51fbbbc0bb8709c93fe89512565c385e7957d1
LucasAVasco <67849851+LucasAVasco@users.noreply.github.com>
2025-03-29 20:46:49 -0300

    feat(bash/libs): add 'linux/arch' library

---
+++ bash/.config/bash/libs/linux/arch.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#
+# Functions related to OS architecture.
+
+linux_arch_amd64='amd64'
+linux_arch_arm32='arm32'
+linux_arch_arm64='arm64'
+
+# Return the current OS architecture.
+#
+# Return on of: 'amd64', 'arm32', 'arm64'
+linux_arch_get() {
+	local uname_res=$(uname -m)
+
+	local arch
+	case "$uname_res" in
+		x86_64)
+			arch="$linux_arch_amd64"
+			;;
+
+		arm64 | aarch64)
+			arch="$linux_arch_arm64"
+			;;
+
+		*)
+			# `uname` returns the arm32 architecture as 'arm<something>'
+			if [[ "$uname_res" =~ ^arm.*$ ]]; then
+				arch="$linux_arch_arm32"
+			else
+				echo 'Error: can not find the architecture!\n' >&2
+				return
+			fi
+			;;
+	esac
+
+	echo -n "$arch"
+}
