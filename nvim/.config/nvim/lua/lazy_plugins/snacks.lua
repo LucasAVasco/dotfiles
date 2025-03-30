9acdf367337f5908b885e068fab2802182055c50
LucasAVasco <67849851+LucasAVasco@users.noreply.github.com>
2025-03-30 20:09:50 -0300

    feat(nvim): enable 'lazygit' plugin of 'snacks.nvim'

--- nvim/.config/nvim/lua/lazy_plugins/snacks.lua
+++ nvim/.config/nvim/lua/lazy_plugins/snacks.lua
@@ -4,10 +4,24 @@ return {
 		priority = 15000,
 		lazy = false,
 
+		cmd = {
+			'Lazygit',
+		},
+
 		---@module 'snacks'
 		---@type snacks.Config
 		opts = {
-			image = { enabled = true },
+			image = {},
+			lazygit = {},
 		},
+
+		config = function(_, opts)
+			local snacks = require('snacks')
+			snacks.setup(opts)
+
+			vim.api.nvim_create_user_command('Lazygit', function()
+				snacks.lazygit.open()
+			end, {})
+		end,
 	},
 }
