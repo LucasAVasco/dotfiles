ea13e120104d66d0a711b7198b2c901cc514e800
LucasAVasco <67849851+LucasAVasco@users.noreply.github.com>
2025-05-11 01:21:02 -0300

    feat(nvim): add 'nvim-bqf' plugin

---
+++ nvim/.config/nvim/lua/lazy_plugins/quickfix.lua
@@ -0,0 +1,18 @@
+return {
+	{
+		'kevinhwang91/nvim-bqf',
+
+		ft = 'qf',
+
+		---@module 'bqf'
+		---@type BqfConfig
+		opts = {
+			-- Overrides
+			func_map = {
+				drop = '<CR>',
+				open = 'o',
+				openc = 'O',
+			},
+		},
+	},
+}
