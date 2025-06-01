6a9766fe17b3663070adf235c56f373157ead10c
LucasAVasco <67849851+LucasAVasco@users.noreply.github.com>
2025-06-01 19:18:34 -0300

    feat(nvim): use 2 spaces as indentation in markdown files

---
+++ nvim/.config/nvim/ftplugin/markdown.lua
@@ -0,0 +1,5 @@
+-- Disables the default style used in markdown files (configured at /usr/share/nvim/runtime/ftplugin/markdown.vim)
+vim.g.markdown_recommended_style = 0
+
+vim.bo.expandtab = true
+vim.bo.shiftwidth = 2
