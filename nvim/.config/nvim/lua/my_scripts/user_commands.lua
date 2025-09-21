ecfec7d32c2e38cc0813d4253c968f3f3508d9ce
LucasAVasco <67849851+LucasAVasco@users.noreply.github.com>
2025-09-21 00:23:42 -0300

    feat(nvim): add commands to insert the current file and folder path

--- nvim/.config/nvim/lua/my_scripts/user_commands.lua
+++ nvim/.config/nvim/lua/my_scripts/user_commands.lua
@@ -15,3 +15,28 @@ end, {
 })
 
 vim.keymap.set('n', '<leader>cc', '<CMD>AppendFileToClip %<CR>', { desc = 'Append current file to clipboard' })
+
+vim.api.nvim_create_user_command('PutPath', function(arguments)
+	-- Absolute of the current file
+	local path = arguments.fargs[1] or vim.fn.expand('%')
+	path = vim.fs.abspath(path)
+
+	vim.api.nvim_put({ path }, 'c', true, true)
+end, {
+	desc = 'Write the current file path',
+	nargs = '?',
+})
+
+vim.api.nvim_create_user_command('PutDir', function(arguments)
+	-- Absolute of the current file
+	local path = arguments.fargs[1] or vim.fn.expand('%')
+	path = vim.fs.abspath(path)
+
+	-- Gets the parent directory
+	path = vim.fs.dirname(path)
+
+	vim.api.nvim_put({ path }, 'c', true, true)
+end, {
+	desc = 'Write the directory of the current file',
+	nargs = '?',
+})
