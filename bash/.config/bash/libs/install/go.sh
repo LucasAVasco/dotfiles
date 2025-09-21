1218ec77599d2cb584c10d81cb7f529d2760e78d
LucasAVasco <67849851+LucasAVasco@users.noreply.github.com>
2025-09-21 04:20:50 -0300

    feat(bash/libs/go): add function to remove binaries installed with Go

--- bash/.config/bash/libs/install/go.sh
+++ bash/.config/bash/libs/install/go.sh
@@ -2,10 +2,28 @@
 #
 # Module to install Golang packages.
 
+source ~/.config/bash/libs/bin.sh
+
 # Install a go package. Requires Go to be installed to work.
 #
 # $1: go package to install.
 install_go_package() {
-	package="$1"
+	local package="$1"
 	go install "$package"
+}
+
+# Remove a go package.
+#
+# $1: go package to remove.
+install_go_remove_package() {
+	local executable="$1"
+
+	# Aborts if it is not installed in a Go directory
+	if [[ $(bin_executable_is_inside_dir_regex "$executable" "go/.*/bin") == n ]]; then
+		return
+	fi
+
+	# Removes the executable
+	local path=$(bin_get_executable_path "$executable")
+	trash "$path"
 }
