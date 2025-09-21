8cd9c3434ce12e83d6969e4930914be796926a9f
LucasAVasco <67849851+LucasAVasco@users.noreply.github.com>
2025-09-21 19:56:33 -0300

    feat(bin/dotfiles_bin_fallback): add 'mise' package

---
+++ bin/.local/dotfiles_bin_fallback/packages/mise.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# Installs `mise` (https://mise.jdx.dev/)
+
+set -e
+
+completions_file="$HOME/.local/share/zsh_completions/_mise"
+
+case "$1" in
+	i | u)
+		# Main command installation
+		curl https://mise.run | sh
+
+		# Completions
+		mise completion zsh > "$completions_file"
+		;;
+
+	r)
+		# Removing Mise and its data
+		test -f ~/.local/bin/mise && mise implode || true
+
+		# Removing completions
+		test -f "$completions_file" && rm "$completions_file" || echo "No completions file found"
+		;;
+esac
