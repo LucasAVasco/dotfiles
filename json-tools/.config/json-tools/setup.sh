c8e7f21a7410ff4d04d11443478665d693b845a8
LucasAVasco <67849851+LucasAVasco@users.noreply.github.com>
2025-09-21 13:07:53 -0300

    refactor(json-tools)!: disable setup if user is not allowed to
    Disable setup script if user is not allowed to install external
    software
    
    BREAKING CHANGE: may disable setup script

--- json-tools/.config/json-tools/setup.sh
+++ json-tools/.config/json-tools/setup.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+#
+# Initial setup for 'json-tools'. Must at least once.
+
+# Does not run this script if the user can not install external software
+[ "$ALLOW_EXTERNAL_SOFTWARE" != 'y' ] && {
+	echo "This script requires external software to be installed." >&2
+	exit 1
+}
 
 # Install dependencies
 luarocks install --local lua-cjson
