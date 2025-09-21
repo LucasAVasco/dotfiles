5b502e5d59dd1e5c82341c4cb4b57a23ddad6fb0
LucasAVasco <67849851+LucasAVasco@users.noreply.github.com>
2025-09-21 06:40:38 -0300

    feat(bash/libs/install/node): add function to remove Node.js packages

--- bash/.config/bash/libs/install/node.sh
+++ bash/.config/bash/libs/install/node.sh
@@ -6,3 +6,10 @@
 install_node_install_package() {
 	npm install -g "$1"
 }
+
+# Remove a binary installed with a NPM package.
+#
+# $1: NPM package.
+install_node_remove_package() {
+	npm uninstall -g "$1"
+}
