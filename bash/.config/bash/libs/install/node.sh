e9f1478d7635b76ef630cd4eff78beff35095bbe
LucasAVasco <67849851+LucasAVasco@users.noreply.github.com>
2025-09-21 06:39:02 -0300

    refactor(bash/lib/install/node)!: rename `install_node_package()`
    New name: `install_node_install_package()`
    
    BREAKING CHANGE: rename `install_node_package()` function

--- bash/.config/bash/libs/install/node.sh
+++ bash/.config/bash/libs/install/node.sh
@@ -2,7 +2,7 @@
 
 # Install a binary from a NPM package
 #
-# $1: NPM package
-install_node_package() {
-	npm install -g "$@"
+# $1: NPM package.
+install_node_install_package() {
+	npm install -g "$1"
 }
