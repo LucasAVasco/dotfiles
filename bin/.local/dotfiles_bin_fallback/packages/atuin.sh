ff1c41cc7a67994e1f2c2ac0100a3daf4a79e306
LucasAVasco <67849851+LucasAVasco@users.noreply.github.com>
2025-10-06 00:20:38 +0000

    feat(bin/dotfiles_bin_fallback/packages): download 'Atuin'
    No longer build Atuin from source, just download it as a tarball and
    extract the binary from it

--- bin/.local/dotfiles_bin_fallback/packages/atuin.sh
+++ bin/.local/dotfiles_bin_fallback/packages/atuin.sh
@@ -4,14 +4,21 @@
 
 set -e
 
-source ~/.config/bash/libs/install/rust.sh
+source ../lib/wget.sh
+
+version=v18.8.0
 
 case "$1" in
 	i | u)
-		install_rust_install_package atuin
+		install_wget_init
+
+		install_wget_download https://github.com/atuinsh/atuin/releases/download/$version/atuin-x86_64-unknown-linux-gnu.tar.gz \
+			atuin.tar.gz
+		install_wget_untar_file atuin.tar.gz
+		wget_export_files_to_bin atuin-x86_64-unknown-linux-gnu/atuin
 		;;
 
 	r)
-		install_rust_remove_package atuin
+		bin_delete_files atuin
 		;;
 esac
