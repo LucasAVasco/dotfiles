c03ae9c28c863785beca518b88b7a1f7e61d4655
LucasAVasco <67849851+LucasAVasco@users.noreply.github.com>
2025-10-06 01:10:33 +0000

    fix(bin/dotfiles_bin_override/lazygit): can not auto install 'lazygit'
    Bug: can not auto-install 'lazygit' with 'dotfiles_bin_fallback'
    packages
    
    Reason: the 'dotfiles_bin_override' overrides the
    'dotfiles_bin_fallback' auto-installers. Instead of calling 'lazygit',
    it calls '/bin/lazygit' (absolute path bypasses the auto-install script)
    
    Solution: the overrides should search for the 'lazygit' binary instead
    of use the absolute path

---
+++ bash/.config/bash/libs/bin_overrides.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+# Library to manage my scripts used override some binaries.
+
+source ~/.config/bash/libs/string.sh
+source ~/.config/bash/libs/bin_fallback.sh
+
+_bin_overrides_path=$(realpath ~/.local/dotfiles_bin_override/)
+
+# Run a command without the influence of the override scripts.
+#
+# $1: command to execute.
+# $2..n: Arguments to the command.
+bin_overrides_call_without_override() {
+	# Must manually install a fallback installer package if it exists (avoids a infinity recursion)
+	if [[ $(bin_fallback_manager has "$1" ) == 'y' ]]; then
+		if [[ $(bin_fallback_manager is-installed "$1") == 'n' ]]; then
+			bin_fallback_manager install "$1"
+		fi
+	fi
+
+	# Ignores the override scripts and executes the command
+	for bin in $(whereis -b lazygit | cut -d: -f 2); do
+		if [[ $(string_has_substring "$bin" "$_bin_overrides_path") == 'n' ]]; then
+			command "$bin" "${@:2}"
+			break
+		fi
+	done
+}
