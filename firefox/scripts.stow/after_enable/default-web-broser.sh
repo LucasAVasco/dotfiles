1d83bd2bbcdf01e2f356247f2ba894fb09d099cf
LucasAVasco <67849851+LucasAVasco@users.noreply.github.com>
2025-10-29 20:15:58 -0300

    feat(firefox): add 'firefox' package
    Set Firefox as default web browser
    
    No longer use 'qutebrowser' as default web browser

---
+++ firefox/scripts.stow/after_enable/default-web-broser.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#
+# Set Firefox as the default web browser.
+
+xdg-settings set default-web-browser firefox.desktop
--- qutebrowser/scripts.stow/once_after_enable/default_web_broser.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-# Set Qutebrowser as the default web browser
-
-# Does not run this script if the user can not install external software
-[ "$ALLOW_EXTERNAL_SOFTWARE" != 'y' ] && exit 0
-
-xdg-settings set default-web-browser org.qutebrowser.qutebrowser.desktop
