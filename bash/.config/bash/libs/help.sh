def7be09e3e1fea55e0534ab818a040952ac66b0
LucasAVasco <67849851+LucasAVasco@users.noreply.github.com>
2025-12-06 13:35:24 -0300

    feat(bash/libs/help): add function to handle help messages from stdin

--- bash/.config/bash/libs/help.sh
+++ bash/.config/bash/libs/help.sh
@@ -32,3 +32,48 @@ help_call_help_function() {
 		exit 0
 	fi
 }
+
+# Remove the indent from a help message.
+#
+# To get the number of indentation that must be removed, this function checks only the first line. All other lines will be formatted using
+# the indentation size of the first line
+#
+# You must not combine spaces and tabs in the help message. Use only one of them
+#
+# stdin: help message
+# stdout: help message without the indent
+help_msg_remove_indent() {
+	local message=$(cat /dev/stdin)
+
+	# Gets the indent size of the first line
+	local first_line=$(echo "$message" | head -n 1)
+	local indent="${first_line%%[^$'\t' ]*}"
+	local indent_size="${#indent}"
+
+	# Removes the indent from all lines
+	printf "%s" "$message" | sed "s/^\s\{${indent_size}\}//g"
+}
+
+# Handle a help message.
+#
+# Automatically print the help message if the user provided the `--help`, `-h` or `help` argument.
+#
+# The help message is formatted to remove the indent (see `help_msg_remove_indent()` function)
+#
+# stdin: help message
+# $1: should the help function be called if the first argument is empty. Accepted values: 'y' or 'n'.
+# $2-n: all command line arguments.
+help_handle() {
+	local help_if_empty="$1"
+	shift
+
+	if [[ "$help_if_empty" == 'y' && "$1" == '' ]]; then
+		cat /dev/stdin | help_msg_remove_indent
+		exit 1
+	fi
+
+	if [[ "$1" == '--help' || "$1" == '-h' || "$1" == 'help' ]]; then
+		cat /dev/stdin | help_msg_remove_indent
+		exit 0
+	fi
+}
