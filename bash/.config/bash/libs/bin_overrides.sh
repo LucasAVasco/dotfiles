640c745a19ae0ffb71966d0c9de6e91f0eb01ffc
LucasAVasco <67849851+LucasAVasco@users.noreply.github.com>
2025-12-06 13:06:10 -0300

    fix(bash/libs/bin_overrides): calling a command without overrides
    Bug: the `bin_overrides_call_without_override()` only calls the
    'lazygit' command, even if the user supplied another one
    
    Reason: the loop that checks by the absolute paths of the executables
    that matches the supplied command is checking only by the 'lazygit'
    command. I hard-coded the lazygit command during the tests and forgot to
    remove it
    
    Solution: remove the hard-coded command and use the supplied one

--- bash/.config/bash/libs/bin_overrides.sh
+++ bash/.config/bash/libs/bin_overrides.sh
@@ -12,6 +12,8 @@ _bin_overrides_path=$(realpath ~/.local/dotfiles_bin_override/)
 # $1: command to execute.
 # $2..n: Arguments to the command.
 bin_overrides_call_without_override() {
+	local cmd="$1"
+
 	# Must manually install a fallback installer package if it exists (avoids a infinity recursion)
 	if [[ $(bin_fallback_manager has "$1" ) == 'y' ]]; then
 		if [[ $(bin_fallback_manager is-installed "$1") == 'n' ]]; then
@@ -20,7 +22,7 @@ bin_overrides_call_without_override() {
 	fi
 
 	# Ignores the override scripts and executes the command
-	for bin in $(whereis -b lazygit | cut -d: -f 2); do
+	for bin in $(whereis -b "$cmd" | cut -d: -f 2); do
 		if [[ $(string_has_substring "$bin" "$_bin_overrides_path") == 'n' ]]; then
 			command "$bin" "${@:2}"
 			break
