a2f7bf1d666aee58a4f306c46c3ccf0e7cfb29b3
LucasAVasco <67849851+LucasAVasco@users.noreply.github.com>
2025-12-14 03:25:57 -0300

    refactor(bash/libs/install/node): use `pnpm` instead of `npm`

--- bash/.config/bash/libs/install/node.sh
+++ bash/.config/bash/libs/install/node.sh
@@ -6,12 +6,12 @@
 #
 # $1: NPM package.
 install_node_install_package() {
-	npm install -g "$1"
+	pnpm install -g "$1"
 }
 
 # Remove a binary installed with a NPM package.
 #
 # $1: NPM package.
 install_node_remove_package() {
-	npm uninstall -g "$1"
+	pnpm uninstall -g "$1"
 }
