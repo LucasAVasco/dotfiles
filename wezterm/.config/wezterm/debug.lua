f70e9ac3b8ac4b0bb4ed3e546d412365a70e660f
LucasAVasco <67849851+LucasAVasco@users.noreply.github.com>
2025-12-14 03:53:17 -0300

    chore(wezterm/debug): update annotation of `DEBUG` global
    The `info`, `warn`, and `error` functions accepts a variable number of
    arguments

--- wezterm/.config/wezterm/debug.lua
+++ wezterm/.config/wezterm/debug.lua
@@ -1,9 +1,9 @@
 local wezterm = require('wezterm')
 
 ---@class DegugUtil Debug utility available in all WezTerm scripts. Use 'ShowDebugOverlay' to show all debug logs
----@field info fun(msg: any) Logs info message
----@field warn fun(msg: any) Logs warning message
----@field error fun(msg: any) Logs error message
+---@field info fun(...: any) Logs info message
+---@field warn fun(...: any) Logs warning message
+---@field error fun(...: any) Logs error message
 DEBUG = {}
 DEBUG.info = wezterm.log_info
 DEBUG.warn = wezterm.log_warn
