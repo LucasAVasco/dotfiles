d7c444ae63b822de14bb8a32d1d92b35d1faeabd
LucasAVasco <67849851+LucasAVasco@users.noreply.github.com>
2025-12-14 03:58:24 -0300

    feat(wezterm/debug): add functions to show notifications and inspect
    The `DEBUG.notify` shows system notifications and the `DEBUG.inspect`
    function converts a value to a string

--- wezterm/.config/wezterm/debug.lua
+++ wezterm/.config/wezterm/debug.lua
@@ -8,3 +8,20 @@ DEBUG = {}
 DEBUG.info = wezterm.log_info
 DEBUG.warn = wezterm.log_warn
 DEBUG.error = wezterm.log_error
+
+---Notify the user
+---@param title? string
+---@param ... string
+function DEBUG.notify(title, ...)
+	local msg = table.concat({ ... }, ' ')
+	title = title or 'WezTerm'
+
+	wezterm.background_child_process({ 'notify-send', title, msg })
+end
+
+---Get the string representation of a value
+---@param ... any
+---@return string
+function DEBUG.inspect(...)
+	return wezterm.to_string(...)
+end
