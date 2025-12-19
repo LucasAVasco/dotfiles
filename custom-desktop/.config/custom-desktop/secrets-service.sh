e47ed25ac01e7b6686962cc1945af2bb43ec56c9
LucasAVasco <67849851+LucasAVasco@users.noreply.github.com>
2025-12-19 16:16:49 -0300

    feat(custom-desktop): add script to start default secrets service

---
+++ custom-desktop/.config/custom-desktop/secrets-service.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#
+# Starts the default freedesktop.org secrets service (see https://specifications.freedesktop.org/secret-service/latest/).
+
+exec ~/.config/secrets/keepassxc-secrets-service.sh
