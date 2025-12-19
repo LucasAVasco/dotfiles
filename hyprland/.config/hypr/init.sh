8c23d3a6bf4fd39e3be3d107e42659e4f21bd70f
LucasAVasco <67849851+LucasAVasco@users.noreply.github.com>
2025-12-19 19:00:13 -0300

    feat(hyrpland/init): start secrets service

--- hyprland/.config/hypr/init.sh
+++ hyprland/.config/hypr/init.sh
@@ -6,5 +6,8 @@
 # Screen locker
 ~/.config/screenlocker/manager.sh enable --no-notify &
 
+# Secrets service
+~/.config/custom-desktop/secrets-service.sh &
+
 # Shows a message if there are running Docker containers
 ~/.config/docker/notify-running-containers.sh &
