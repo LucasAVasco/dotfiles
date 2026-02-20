764646f91666ad2b160f54c8e90a7e985558354f
LucasAVasco <67849851+LucasAVasco@users.noreply.github.com>
2026-02-20 14:32:58 -0300

    feat(bin/dotfiles_bin_fallback/packages): add 'kompose' package

---
+++ bin/.local/dotfiles_bin_fallback/packages/kompose.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# Kompose (https://kompose.io/) package.
+
+set -e
+
+version='v1.38.0'
+
+source ../lib/wget.sh
+source ~/.config/bash/libs/linux/arch.sh
+
+case "$1" in
+	i | u)
+		install_wget_init
+		install_wget_download "https://github.com/kubernetes/kompose/releases/download/$version/kompose-linux-$(linux_arch_get)" 'kompose'
+		wget_export_files_to_bin kompose
+		;;
+
+	r)
+		bin_delete_files kompose
+		;;
+esac
