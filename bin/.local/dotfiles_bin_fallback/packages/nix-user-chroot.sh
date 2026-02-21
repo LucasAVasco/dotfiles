f979e3162595735218e1994c331d91289d446612
LucasAVasco <67849851+LucasAVasco@users.noreply.github.com>
2026-02-21 11:00:38 -0300

    feat(bin/dotfiles_bin_fallback/packages): add 'nix-user-chroot' package

---
+++ bin/.local/dotfiles_bin_fallback/packages/nix-user-chroot.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# nix-user-chroot (https://github.com/nix-community/nix-user-chroot) package.
+
+set -e
+
+source ~/.config/bash/libs/install/rust.sh
+
+case "$1" in
+	i | u)
+		install_rust_install_package nix-user-chroot
+
+		# From the official documentation at https://github.com/nix-community/nix-user-chroot (I just added the '-p' flag to `mkdir` and set
+		# the permission flags to 0700):
+		mkdir -p -m 0700 ~/.nix
+		nix-user-chroot ~/.nix bash -c "curl -L https://nixos.org/nix/install | bash"
+		;;
+
+	r)
+		install_rust_remove_package nix-user-chroot
+		;;
+esac
