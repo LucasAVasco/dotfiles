9c08ca394f6d88bbe1fd77dc60532f31f27da24c
LucasAVasco <67849851+LucasAVasco@users.noreply.github.com>
2026-02-24 12:49:19 -0300

    feat(custom_scripts/libs): add general library for custom scripts

---
+++ custom_scripts/.local/custom_scripts/libs/scripts.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+# General functions to use in custom scripts
+
+source ~/.config/bash/libs/log.sh
+
+# Change the directory to the one that the user was when the CLI was called
+scripts_cd_to_invoke_dir() {
+	cd "$CUSTOM_SCRIPT_INVOKE_DIR"
+}
+
+# Log an info message.
+#
+# $1: message.
+#
+# stdout: formatted message.
+scripts_log_info() {
+	log_info "$@"
+}
+
+# Log an error message and exit with an error code.
+#
+# $1: message.
+# $2: error code. Optional. Default: 1
+#
+# stdout: formatted message.
+scripts_log_error() {
+	log_error "$@"
+}
