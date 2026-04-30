aef3ff5a345fee23f623534a189e35c6389d9889
LucasAVasco <67849851+LucasAVasco@users.noreply.github.com>
2026-04-30 13:07:28 -0300

    feat(nvim/lazy_plugins/filetype/helm): remove 'towolf/vim-helm' plugin
    This plugin provides vim syntax for Helm templates. We no longer need
    it because tree-sitter now supports Helm templates
    
    Move the Helm file-type configuration to the
    './nvim/.config/nvim/lua/my_scripts/file_types.lua' script

--- nvim/.config/nvim/lua/my_scripts/file_types.lua
+++ nvim/.config/nvim/lua/my_scripts/file_types.lua
@@ -7,3 +7,29 @@ vim.filetype.add({
 		['vifm'] = 'vim',
 	},
 })
+
+---Filetype function that checks if the file is a Helm related file.
+---@param path string File path.
+---@return string? file_type 'helm' if the file is a Helm related file. None otherwise.
+local function is_helm_template_file(path)
+	local helm_base_dir = MYFUNC.iter_path(path, function(dir)
+		return vim.fn.filereadable(dir .. '/Chart.yaml') == 1
+	end)
+
+	if helm_base_dir then
+		return 'helm'
+	end
+end
+
+vim.filetype.add({
+	filename = {
+		['Chart.yaml'] = 'helm',
+		['Chart.lock'] = 'helm',
+	},
+
+	pattern = {
+		['helmfile.*.ya?ml(.gotmpl)'] = 'helm',
+		['.*/templates/.*.ya?ml'] = is_helm_template_file,
+		['.*/templates/.*.tpl'] = is_helm_template_file,
+	},
+})
