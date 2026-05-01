5a8415d5670fe420a10e2d678da8a84130cea770
LucasAVasco <67849851+LucasAVasco@users.noreply.github.com>
2026-05-01 20:33:43 -0300

    refactor(nvim/my_configs/LSP/yamlls): move schema config to its own file

---
+++ nvim/.config/nvim/lua/my_plugin_libs/yamlls/schema.lua
@@ -0,0 +1,28 @@
+local schemastore = require('schemastore')
+local schemas = schemastore.yaml.schemas()
+
+-- Configure the patterns for kubernetes files
+
+schemas.kubernetes = {
+	'templates/**', -- Helm templates
+	'k8s/*.yaml',
+	'k8s-*.yaml',
+	'k3s/*.yaml',
+	'k3s-*.yaml',
+	'k8s/*.yml',
+	'k8s-*.yml',
+	'k3s/*.yml',
+	'k3s-*.yml',
+
+	-- Editing an existing instance with `kubectl edit`
+	'kubectl-edit-*.yaml',
+	'kubectl-edit-*.yml',
+}
+
+-- Support to 'helmfile.yml.gotmpl' and 'helmfile.yaml.gotmpl'
+
+local helmfile_schema = schemas['https://www.schemastore.org/helmfile.json']
+table.insert(helmfile_schema, 'helmfile.yml.gotmpl')
+table.insert(helmfile_schema, 'helmfile.yaml.gotmpl')
+
+return schemas
