0124d84936f25bfac2b9a04299764f16ae75803c
LucasAVasco <67849851+LucasAVasco@users.noreply.github.com>
2026-05-01 20:08:42 -0300

    feat(nvim/my_configs/LSP/jsonls): extend VSCode Snippets matches
    Configure Jsonls to recognize my custom VSCode snippets at
    '~/.config/nvim/vscode_snippets/' as VSCode snippets

--- nvim/.config/nvim/lua/my_configs/LSP/configs/jsonls.lua
+++ nvim/.config/nvim/lua/my_configs/LSP/configs/jsonls.lua
@@ -1,4 +1,27 @@
+local catalog = require('schemastore.catalog')
 local schemastore = require('schemastore')
+
+---Create a schema that extends another schema with additional file matches
+---@param schema_name string The name of the schema to extend
+---@param matches string[] The file matches to append to the schema
+---@return SchemaEntry?
+local function extend_schema(schema_name, matches)
+	local index = catalog.json.index[schema_name]
+	if index == nil then
+		vim.notify('Schema not found: ' .. schema_name, vim.log.levels.ERROR)
+		return
+	end
+
+	local schema = vim.deepcopy(catalog.json.schemas[index])
+	MYFUNC.array_concat(schema.fileMatch, matches)
+	return schema
+end
+
+local schemas = schemastore.json.schemas({
+	extra = {
+		extend_schema('VSCode Code Snippets', { 'vscode_snippets/*/*.json' }),
+	},
+})
 
 ---@module "my_configs.LSP.types"
 ---@type my_configs.LSP.LspServerConfig
@@ -7,7 +30,7 @@ local Config = {
 		---@diagnostic disable-next-line: missing-fields
 		json = {
 			validate = { enable = true },
-			schemas = schemastore.json.schemas(),
+			schemas = schemas,
 		},
 	},
 }
