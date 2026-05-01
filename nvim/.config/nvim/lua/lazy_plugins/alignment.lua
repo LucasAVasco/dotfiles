52ff2a58b1e5406549d23e8f973a27e72ff171e8
LucasAVasco <67849851+LucasAVasco@users.noreply.github.com>
2026-05-01 15:56:16 -0300

    feat(nvim/lazy_plugins/alignment): add easy align alias
    Add aliases for easy align commands ignoring `easy_align_ignore_groups`
    configurations

--- nvim/.config/nvim/lua/lazy_plugins/alignment.lua
+++ nvim/.config/nvim/lua/lazy_plugins/alignment.lua
@@ -18,6 +18,15 @@ return {
 	{
 		'junegunn/vim-easy-align',
 
+		cmd = {
+			'EasyAlign',
+			'LiveEasyAlign',
+
+			-- My commands
+			'EasyAlignNoIgnore',
+			'LiveEasyAlignNoIgnore',
+		},
+
 		keys = {
 			{ '<CR>', '<Plug>(LiveEasyAlign)', mode = 'x', desc = 'LiveEasyAlign' },
 			{ '<leader>ga', '<Plug>(EasyAlign)', mode = { 'n', 'x' }, desc = 'EasyAlign' },
@@ -25,6 +34,23 @@ return {
 
 		init = function()
 			vim.g.easy_align_ignore_unmatched = 1 -- Ignore lines with out delimiters
+		end,
+
+		config = function(_, _)
+			---Creates an alias for an easy_align command without `easy_align_ignore_groups`
+			---@param command string The easy_align command to alias
+			---@param name string The name of the alias
+			local function create_easy_align_alias_without_ignore_group(command, name)
+				vim.api.nvim_create_user_command(name, function(args)
+					local backup_group = vim.g.easy_align_ignore_groups
+					vim.g.easy_align_ignore_groups = {}
+					vim.cmd({ cmd = command, args = args.fargs, bang = args.bang, range = { args.line1, args.line2 } })
+					vim.g.easy_align_ignore_groups = backup_group
+				end, { nargs = '*', range = true, bang = true })
+			end
+
+			create_easy_align_alias_without_ignore_group('EasyAlign', 'EasyAlignNoIgnore')
+			create_easy_align_alias_without_ignore_group('LiveEasyAlign', 'LiveEasyAlignNoIgnore')
 		end,
 	},
 }
