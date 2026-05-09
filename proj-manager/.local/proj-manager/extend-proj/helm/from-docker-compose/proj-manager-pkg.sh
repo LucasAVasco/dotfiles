b2d17b86b9c48b223901ac1102789eeec9c6d5fe
LucasAVasco <67849851+LucasAVasco@users.noreply.github.com>
2026-05-08 23:09:27 -0300

    feat(proj-manager/extend-proj/helm): add script to convert compose files
    Add script to convert docker compose files to Helm templates

---
+++ proj-manager/.local/proj-manager/extend-proj/helm/from-docker-compose/proj-manager-pkg.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+# Convert Docker Compose files to Helm templates
+
+set -e
+
+source ~/.config/bash/libs/dialog/dialog.sh
+source ~/.local/proj-manager/libs/package.sh
+
+package_cd_to_invoke_dir
+
+helm_dir=$(dialog_ask_input 'Output Helm directory path')
+if [[ -z "$helm_dir" ]]; then
+	echo 'Output directory not provided. Aborting...'
+	exit 1
+fi
+
+# Write the Helm templates to this directory before copying them to the Helm directory
+tmp_helm_dir=$(mktemp -d)
+trap "trash $tmp_helm_dir" EXIT
+
+package_log "Selecting Docker Compose files..."
+compose_files=$(fd 'compose.yaml' | fzf --preview 'pretty-preview {}' --multi)
+
+package_log 'Converting Docker Compose files to Helm templates...'
+# cSpell:words kompose
+kompose convert --with-kompose-annotation=false --controller deployment -c -o "$tmp_helm_dir" \
+	$(echo -n "$compose_files" | foreach-line echo -f /_)
+
+package_log 'Copying generated templates to helm directory...'
+mv -i "$tmp_helm_dir"/templates/* "$helm_dir"/templates/
