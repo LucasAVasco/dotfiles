16599a4600727c8c320a286900cf33462e6d210a
LucasAVasco <67849851+LucasAVasco@users.noreply.github.com>
2026-05-09 00:27:22 -0300

    feat(proj-manager/update-proj/kube): add script to move envs to config
    Add script to move environment variables from a container definition to
    a config map in the same file (append to the end of the file)

---
+++ proj-manager/.local/proj-manager/update-proj/kube/move-env-to-config-map/proj-manager-pkg.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+# Move environment variables from a container definition to a config map in the same file (append to the end of the file)
+
+set -e
+
+source ~/.config/bash/libs/dialog/dialog.sh
+source ~/.local/proj-manager/libs/package.sh
+
+package_cd_to_invoke_dir
+
+file_path=$(dialog_ask_selection "Select file to extract env from" $(fd '\.yaml'))
+index=$(dialog_ask_input "Index of the container to extract env from")
+
+# Extract env
+envs=$(yq ".spec.template.spec.containers.[$index].env" "$file_path" | \
+	yq eval '.[] as $item | "\($item.name): \($item.value)"')
+
+# Create config map without data
+cat <<EOF >> "$file_path"
+---
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: replace-me
+data:
+EOF
+
+# Add environment variables as data
+printf "%s" "$envs" | sed 's/^/  /' >> "$file_path"
