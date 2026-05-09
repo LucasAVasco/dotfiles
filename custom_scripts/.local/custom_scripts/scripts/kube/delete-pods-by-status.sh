27c594b4ed7f482ec49b3d0d6b10dc0d09885255
LucasAVasco <67849851+LucasAVasco@users.noreply.github.com>
2026-05-09 00:00:18 -0300

    feat(custom_scripts/scripts/kube): add 'delete-pods-by-status' script

---
+++ custom_scripts/.local/custom_scripts/scripts/kube/delete-pods-by-status.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# Deletes all pods based on their status
+
+source ~/.config/bash/libs/dialog/dialog.sh
+
+status_filter=$(dialog_ask_selection "Select the status of the pods to delete" 'Failed (ended with error)' 'Succeeded (completed)')
+status_filter=${status_filter%% *} # Remove content after spaces
+
+# Ask confirmation
+if [[ $(dialog_ask_boolean "Delete $status_filter pods?" n) == n ]]; then
+	echo 'Aborted' >&2
+	exit
+fi
+
+# Delete pods
+kubectl get pods --all-namespaces \
+	--field-selector=status.phase="$status_filter" \
+	-o json | jq -r '.items[] | "\(.metadata.namespace) \(.metadata.name)"' \
+	| while read ns pod; do
+		kubectl delete pod --namespace "$ns" "$pod"
+	done
