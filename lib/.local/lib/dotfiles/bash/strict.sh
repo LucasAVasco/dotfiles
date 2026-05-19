09185ba390838fd961aa4da9c1b635183cd710b4
LucasAVasco <67849851+LucasAVasco@users.noreply.github.com>
2026-05-18 23:15:19 -0300

    feat(lib/bash): add 'strict' library
    Library to enable bash strict mode

---
+++ lib/.local/lib/dotfiles/bash/strict.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# Enable bash strict mode
+#
+# - Exit immediately if a command exits with a non-zero status
+# - Exit if an undefined variable is used
+# - Exit if a pipeline returns a non-zero status
+
+set -euo pipefail
