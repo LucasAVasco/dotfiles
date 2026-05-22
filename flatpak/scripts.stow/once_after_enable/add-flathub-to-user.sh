8b92f1e240e53fff48a76f05b1ab98688fa1de89
LucasAVasco <67849851+LucasAVasco@users.noreply.github.com>
2026-05-22 05:51:34 -0300

    fix(flatpak/once_after_enable/add-flathub-to-user.sh): add flathub
    The old script added the flathub remote without checking if flatpak was
    installed
    
    Do nothing if flatpak is not installed

--- flatpak/scripts.stow/once_after_enable/add-flathub-to-user.sh
+++ flatpak/scripts.stow/once_after_enable/add-flathub-to-user.sh
@@ -2,7 +2,14 @@
 #
 # Add the flathub repositories to the list of repositories for the user.
 
+set -eof pipefail
+
 [ "$ALLOW_EXTERNAL_SOFTWARE" != 'y' ] && exit
+
+if ! command -v flatpak >/dev/null 2>&1; then
+	echo "can not add flathub remote: flatpak is not installed"
+	exit
+fi
 
 # From the official arch wiki documentation at 'https://wiki.archlinux.org/title/Flatpak':
 flatpak remote-add --if-not-exists --user flathub https://dl.flathub.org/repo/flathub.flatpakrepo
