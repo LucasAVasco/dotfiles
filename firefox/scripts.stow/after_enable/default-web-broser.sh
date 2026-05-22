e96dd89e45a7a9405a6ad5e95e3ac97e50a1d9fa
LucasAVasco <67849851+LucasAVasco@users.noreply.github.com>
2026-05-22 05:48:26 -0300

    fix(firefox/after_enable/default-web-browser.sh): check is is installed
    The old script set the default web browser to Firefox without checking
    if Firefox is installed
    
    Do nothing if Firefox is not installed

--- firefox/scripts.stow/after_enable/default-web-broser.sh
+++ firefox/scripts.stow/after_enable/default-web-broser.sh
@@ -2,4 +2,11 @@
 #
 # Set Firefox as the default web browser.
 
+set -eof pipefail
+
+if ! command -v firefox >/dev/null 2>&1; then
+	echo "Can not set Firefox as the default web browser: firefox is not installed."
+	exit
+fi
+
 xdg-settings set default-web-browser firefox.desktop
