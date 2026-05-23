8582afb31dc2939c4a87e83fa44cb1cc458e3881
LucasAVasco <67849851+LucasAVasco@users.noreply.github.com>
2026-05-22 23:13:40 -0300

    feat(lib/bash/security/external_software): add function to exit script
    Add function to exit if the user can not install external software

--- lib/.local/lib/dotfiles/bash/security/external_software.sh
+++ lib/.local/lib/dotfiles/bash/security/external_software.sh
@@ -3,7 +3,17 @@
 # Library to check if external software is allowed.
 
 security_external_software_allowed=n
-
-if [[ "$ALLOW_EXTERNAL_SOFTWARE" == 'y' ]]; then
+if [[ "${ALLOW_EXTERNAL_SOFTWARE:-}" == 'y' ]]; then
 	security_external_software_allowed=y
 fi
+
+# Exit if the user can not install external software
+#
+# $1: Error message. Optional. Default: ''
+# $2: Error code. Optional. Default: 0
+security_external_software_exit_if_not_allowed() {
+	if [[ $security_external_software_allowed == n ]]; then
+		printf '%s\n' "${1:-}"
+		exit "${2:-0}"
+	fi
+}
