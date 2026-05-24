e6e94b02cd2a035e76ef23733e34d13e703df201
LucasAVasco <67849851+LucasAVasco@users.noreply.github.com>
2026-05-23 23:57:12 -0300

    fix(bin/dotfiles_bin_fallback/packages/opencode): run 'postinstall.mjs'
    Opencode now has a post-install script

--- bin/.local/dotfiles_bin_fallback/packages/opencode.sh
+++ bin/.local/dotfiles_bin_fallback/packages/opencode.sh
@@ -9,6 +9,7 @@ source ~/.local/lib/dotfiles/bash/install/node.sh
 case "$1" in
 	i | u)
 		install_node_install_package opencode-ai
+		node "$(pnpm root -g)/opencode-ai/postinstall.mjs"
 		;;
 
 	r)
