6d0eea0f6a1d849252f360601ebb74f19deeb123
LucasAVasco <67849851+LucasAVasco@users.noreply.github.com>
2026-05-26 00:30:13 -0300

    feat(zsh/apps/mise): activate 'mise' when it is installed
    If can not immediately activate 'mise', it will be activated when it is
    installed

--- zsh/.config/zsh/apps/mise.zsh
+++ zsh/.config/zsh/apps/mise.zsh
@@ -1,4 +1,13 @@
 # Does not run this scripts if the user can not install external software
 [ "$ALLOW_EXTERNAL_SOFTWARE" != 'y' ] && return
 
-test -f ~/.local/bin/mise && eval "$(~/.local/bin/mise activate zsh)"
+if [[ -f ~/.local/bin/mise ]]; then
+	eval "$(~/.local/bin/mise activate zsh)"
+else
+	# Activate 'mise' when it is installed
+	add-zsh-hook precmd __try_activate_mise
+	__try_activate_mise() {
+		[[ -f ~/.local/bin/mise ]] && eval "$(~/.local/bin/mise activate zsh)"
+		add-zsh-hook -d precmd __try_activate_mise # Remove the hook
+	}
+fi
