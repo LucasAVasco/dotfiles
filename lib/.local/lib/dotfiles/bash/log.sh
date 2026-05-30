fab935cedfff3fc90a24f9b733cd635ee2fc0034
LucasAVasco <67849851+LucasAVasco@users.noreply.github.com>
2026-05-30 19:48:40 -0300

    feat(lib/bash/log): add 'log_warn' function

--- lib/.local/lib/dotfiles/bash/log.sh
+++ lib/.local/lib/dotfiles/bash/log.sh
@@ -11,6 +11,15 @@ log_info() {
 	echo -e "\n\033[0;44m INFO \033[0m $1\n"
 }
 
+# Log an warning message.
+#
+# $1: message.
+#
+# stdout: formatted message.
+log_warn() {
+	echo -e "\n\033[0;43m\033[1;30m WARNING \033[0m $1\n"
+}
+
 # Log an error message and exit with an error code.
 #
 # $1: message.
