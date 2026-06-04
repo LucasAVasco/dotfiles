bddbab96b39cb09ceb85646a97c74bcfdb820121
LucasAVasco <67849851+LucasAVasco@users.noreply.github.com>
2026-06-04 19:47:09 -0300

    feat(nvim/lazy_plugins/snacks): align image preview to the right
    Align the image preview of 'snacks image' to the right of the window to
    avoid overlapping with the code being edited (math equations, etc.)

--- nvim/.config/nvim/lua/lazy_plugins/snacks.lua
+++ nvim/.config/nvim/lua/lazy_plugins/snacks.lua
@@ -25,6 +25,42 @@ return {
 			picker = {
 				ui_select = true,
 			},
+
+			styles = {
+				-- Move the image preview to the right of the window. This is useful when the preview is over the code being edited (math
+				-- equations, etc.). Try to use the same vertical position as the cursor. If the cursor is too close to the bottom of the
+				-- window, it will be moved up
+				snacks_image = {
+					relative = 'win',
+					position = 'float',
+					row = function(win)
+						local win_id = vim.api.nvim_get_current_win()
+						local cursor = vim.api.nvim_win_get_cursor(win_id)
+						local screen_cursor = vim.fn.screenpos(win_id, cursor[1], cursor[2] + 1)
+						local screen_cursor_row = screen_cursor.row -- Row of the cursor relative to the screen
+						screen_cursor_row = screen_cursor_row - 2 -- Remove the buffer line (bufferline.nvim) and drop bar (dropbar.nvim)
+
+						-- Height of the popup
+						local popup_height = win.opts.height
+						if not popup_height then
+							popup_height = vim.api.nvim_win_get_height(win_id)
+						elseif type(popup_height) == 'function' then
+							popup_height = popup_height(win)
+						end
+						popup_height = popup_height + 2 -- Add the borders size
+
+						-- Height of the main window (where the cursor is and the image will be displayed)
+						local window_height = vim.api.nvim_win_get_height(0)
+						if screen_cursor_row + popup_height > window_height then
+							return window_height - popup_height
+						end
+
+						return screen_cursor_row
+					end,
+
+					col = -2,
+				},
+			},
 		},
 
 		config = function(_, opts)
