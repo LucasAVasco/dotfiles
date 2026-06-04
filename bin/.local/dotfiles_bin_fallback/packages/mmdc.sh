9a133825c6d71aa951dd2480607ed599eefe8f62
LucasAVasco <67849851+LucasAVasco@users.noreply.github.com>
2026-06-04 18:08:46 -0300

    fix(bin/dotfiles_bin_fallback/packages/mmdc): install chrome-headless
    The mmdc.sh package installed mermaid-cli but not chrome-headless-shell.
    This causes mmdc.sh to fail with the following error message:
    
    ```
    Error: Could not find Chrome (ver. 148.0.7778.97). This can occur if either
     1. you did not perform an installation before running the script (e.g. `npx puppeteer browsers install chrome-headless-shell`) or
     2. your cache path is incorrectly configured (which is: /home/lucas_dev/.cache/puppeteer).
    For (2), check out our guide on configuring puppeteer at https://pptr.dev/guides/configuration.
    ```
    
    Update the installation script to install the required
    chrome-headless-shell version

--- bin/.local/dotfiles_bin_fallback/packages/mmdc.sh
+++ bin/.local/dotfiles_bin_fallback/packages/mmdc.sh
@@ -6,9 +6,15 @@ set -e
 
 source ~/.local/lib/dotfiles/bash/install/node.sh
 
+# NOTE(LucasAVasco): mermaid-cli requires a specific version of chrome-headless-shell. You need to run it and check the error message to get
+# the correct version
+mermaid_cli_version='11.15.0'
+chrome_headless_shell_version='148.0.7778.97'
+
 case "$1" in
 	i | u)
-		install_node_install_package '@mermaid-js/mermaid-cli'
+		install_node_install_package "@mermaid-js/mermaid-cli@$mermaid_cli_version"
+		pnpx puppeteer browsers install "chrome-headless-shell@$chrome_headless_shell_version"
 		;;
 
 	r)
