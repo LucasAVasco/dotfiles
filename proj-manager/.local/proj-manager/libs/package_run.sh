6208d2af5f4697f4f7f7e77b8847614ccafdd47b
LucasAVasco <67849851+LucasAVasco@users.noreply.github.com>
2026-06-10 23:06:09 -0300

    refactor(proj-manager/libs/package_run): run provider in its own folder

--- proj-manager/.local/proj-manager/libs/package_run.sh
+++ proj-manager/.local/proj-manager/libs/package_run.sh
@@ -14,13 +14,16 @@ package_run_proj_manager_file_basename="proj-manager-pkg.sh"
 #
 # stdout: The output of the provider script
 package_run_provider() {
-	local project_folder="$1"
-	if [[ $(paths_is_abs "$project_folder") == n ]]; then
-		project_folder="$HOME/.local/proj-manager/$project_folder"
+	local provider_folder="$1"
+	if [[ $(paths_is_abs "$provider_folder") == n ]]; then
+		provider_folder="$HOME/.local/proj-manager/$provider_folder"
 	fi
-	project_folder=$(realpath -m "$project_folder")
+	provider_folder=$(realpath -m "$provider_folder")
 
-	local script="$project_folder/$package_run_proj_manager_file_basename"
+	local script="$provider_folder/$package_run_proj_manager_file_basename"
 	package_log "Running provider '$script' with arguments '${@:2}'"
-	PM_PROVIDER_DIR="$project_folder" "$script" run "${@:2}"
+	(
+		cd "$provider_folder"
+		PM_PROVIDER_DIR="$provider_folder" "$script" run "${@:2}"
+	)
 }
